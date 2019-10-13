3c199a206eb37ce18cf9679843f3c64915756cc0
Ebin Xavier <ebinx7@gmail.com>
2019-10-13 13:58:39 +0400

    auto build test

--- src/waterJug.js
+++ src/waterJug.js
@@ -35,6 +35,7 @@ return new Promise((resolve, reject)=>{
             // 3 = B=FILL
             // 4 = A=EMPTY
             // 5 = B=EMPTY
+            //test
           let traceElement='';
           switch(dir){
             case 0:
